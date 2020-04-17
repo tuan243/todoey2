5185426700a5700846367e7da6e808648882cc50
Đỗ Minh Tuấn <tgbao@vlab.tech>
2020-04-17 17:05:21 +0700

    convert to mvc design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by mac on 4/17/20.
+//  Copyright © 2020 VLAB J.S.C. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
